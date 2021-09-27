774ed9f20fd94ccad3d41cc2e5eda140a7c0d5ab
Pieter <9167905+Pieterv24@users.noreply.github.com>
2021-09-27 19:46:11 +0200

    removed weird files

--- keyboards/checkerboards/axon40/keymaps/vial/config.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0x8C, 0xFF, 0x67, 0x28, 0x9D, 0x84, 0xA0, 0xB0}
-
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 16 }
--- keyboards/checkerboards/axon40/keymaps/vial/readme.md
+++
@@ -1,1 +0,0 @@
-# The VIAL default layout
