ac598c807e5798835613204c18471c30f7b73ce0
Pieter <9167905+Pieterv24@users.noreply.github.com>
2021-10-19 13:08:27 +0200

    added VIAL keymap to XD87

---
+++ keyboards/xiudi/xd87/keymaps/vial/rules.mk
@@ -0,0 +1,12 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+LTO_ENABLE = yes
+QMK_SETTINGS = yes
+TAP_DANCE_ENABLE = no
+COMBO_ENABLE = no
+
+BACKLIGHT_ENABLE = yes
+CONSOLE_ENABLE = no		# Console for debug
+COMMAND_ENABLE = no	    # Commands for debug and configuration
+NKRO_ENABLE = yes		# USB Nkey Rollover
+
