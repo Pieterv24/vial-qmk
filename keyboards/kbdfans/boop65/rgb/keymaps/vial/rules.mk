9a176cc31f939749a35cbd3fca7f43c7cedcd874
Pieter <9167905+Pieterv24@users.noreply.github.com>
2022-01-12 10:27:18 +0100

    updated boop65 vial config

--- keyboards/kbdfans/boop65/rgb/keymaps/vial/rules.mk
+++ keyboards/kbdfans/boop65/rgb/keymaps/vial/rules.mk
@@ -2,13 +2,13 @@ VIA_ENABLE = yes
 VIAL_ENABLE = yes
 LTO_ENABLE = yes
 
-TAP_DANCE_ENABLE = no
-QMK_SETTINGS = yes
-COMBO_ENABLE = no
+TAP_DANCE_ENABLE = yes
+QMK_SETTINGS = no
+COMBO_ENABLE = yes
 
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = no       # Audio control and System control
+EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
@@ -17,4 +17,4 @@ RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 RGB_MATRIX_ENABLE = yes        # Use RGB matrix
 RGB_MATRIX_DRIVER = IS31FL3741
-VIAL_RGB_ENABLE = yes
+VIALRGB_ENABLE = yes
