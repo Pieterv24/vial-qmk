f4334534d781bd6a0deb2d969656cf700071f647
Pieter <9167905+Pieterv24@users.noreply.github.com>
2023-12-30 18:13:35 +0100

    Added the Nein Pad

---
+++ keyboards/0_pieterv24/nein_pad/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+// Copyright 2023 Pieter Verweij (@Pieterv24)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x32, 0xF1, 0x42, 0xEC, 0xB5, 0x7F, 0x8B, 0xBC}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
---
+++ keyboards/0_pieterv24/nein_pad/rules.mk
@@ -0,0 +1,9 @@
+# Build Options
+#   change yes to no to disable
+#
+LTO_ENABLE = yes
+
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
