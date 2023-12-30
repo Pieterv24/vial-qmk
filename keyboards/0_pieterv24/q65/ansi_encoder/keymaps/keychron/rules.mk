e322a586b84995e7c9d105aa4c6446817b7e01f6
Pieter <9167905+Pieterv24@users.noreply.github.com>
2023-12-30 12:01:32 +0100

    Updated Q65 config

--- keyboards/0_pieterv24/q65/ansi_encoder/keymaps/keychron/rules.mk
+++
@@ -1,5 +0,0 @@
-VIA_ENABLE = yes
-ENCODER_MAP_ENABLE = yes
-
-VPATH += keyboards/0_pieterv24/keychron_common
-SRC += keychron_common.c
--- keyboards/0_pieterv24/q65/ansi_encoder/keymaps/vial/rules.mk
+++ keyboards/0_pieterv24/q65/ansi_encoder/keymaps/vial/rules.mk
@@ -3,5 +3,5 @@ VIAL_ENABLE = yes
 VIALRGB_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
 
-VPATH += keyboards/0_pieterv24/keychron_common
+VPATH += keyboards/0_pieterv24/common/keychron
 SRC += keychron_common.c
