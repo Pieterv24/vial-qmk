b3b99c7fa190b7088a0f3c62a292c1bccde45a5e
Pieter <9167905+Pieterv24@users.noreply.github.com>
2024-04-08 10:12:17 +0200

    add signalrgb q1

---
+++ keyboards/pieterv24/q1/ansi_encoder/keymaps/vial_funswitch_signalrgb/config.h
@@ -0,0 +1,10 @@
+// Copyright 2023 Pieter Verweij (@Pieterv24)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x70, 0x39, 0xC4, 0x51, 0xF7, 0xF5, 0x4A, 0x1E}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+
+#define VIAL_ENCODER_DEFAULT { KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS }
