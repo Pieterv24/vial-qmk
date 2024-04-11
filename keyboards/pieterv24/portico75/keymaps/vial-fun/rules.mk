76e4718fe5315a96e22d40d6a5183bfe0efb883a
Pieter <9167905+Pieterv24@users.noreply.github.com>
2024-04-11 12:56:15 +0200

    updated protico

---
+++ keyboards/pieterv24/portico75/keymaps/vial-fun/rules.mk
@@ -0,0 +1,14 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+VIALRGB_ENABLE = yes
+
+QMK_SETTINGS = no
+TAP_DANCE_ENABLE = no
+COMBO_ENABLE = no
+KEY_OVERRIDE_ENABLE = no
+
+ENCODER_MAP_ENABLE = yes
+
+VPATH += keyboards/pieterv24/common/idiot_type
+SRC += idiot_type.c
