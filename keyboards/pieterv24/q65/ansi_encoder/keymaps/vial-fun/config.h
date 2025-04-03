544b24bec394bfae021179d3c900914921b09334
Pieterv24 <9167905+Pieterv24@users.noreply.github.com>
2025-04-03 15:07:02 +0200

    Updated q65

---
+++ keyboards/pieterv24/q65/ansi_encoder/keymaps/vial-fun/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x0E, 0x4E, 0x06, 0x21, 0x8A, 0x02, 0xAB, 0x00}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 14 }
+
+#undef DYNAMIC_KEYMAP_LAYER_COUNT
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
