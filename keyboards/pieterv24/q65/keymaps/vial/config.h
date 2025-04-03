154f6ebf242e122b51c530c335029a04181c980f
Pieterv24 <9167905+Pieterv24@users.noreply.github.com>
2025-04-03 15:07:19 +0200

    Updated removed files

--- keyboards/pieterv24/q65/keymaps/vial/config.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0x3A, 0xA9, 0xAD, 0xF7, 0xE0, 0xA6, 0xBC, 0x63}
-
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 3 }
