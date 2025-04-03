e6287fc268db31f5554087c0e653b255b7e3d4bc
Pieterv24 <9167905+Pieterv24@users.noreply.github.com>
2025-04-03 15:00:01 +0200

    Updated kbd67_tofu

---
+++ keyboards/pieterv24/kbd67_tofu/keymaps/vial-fun/config.h
@@ -0,0 +1,13 @@
+// Copyright 2023 Pieter Verweij (@Pieterv24)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA4, 0x35, 0x84, 0x61, 0xA2, 0xEC, 0xDA, 0x11}
+
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 15}
+
+#define VIAL_TAP_DANCE_ENTRIES 4
+#define VIAL_COMBO_ENTRIES 4
+#define VIAL_KEY_OVERRIDE_ENTRIES 4
---
+++ keyboards/pieterv24/kbd67_tofu/keymaps/vial-fun/rules.mk
@@ -0,0 +1,5 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+VPATH += keyboards/pieterv24/common/idiot_type
+SRC += idiot_type.c
