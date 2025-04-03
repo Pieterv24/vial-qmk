97cc27fe3675599f18114aaff8edcc385c7fdcb5
Pieterv24 <9167905+Pieterv24@users.noreply.github.com>
2025-04-03 13:54:18 +0200

    Added keychron q11 to personal folder

---
+++ keyboards/pieterv24/q11/ansi_encoder/keymaps/vial-fun/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x86, 0x7E, 0x96, 0x5E, 0xA0, 0xA6, 0x5A, 0x7E}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 9 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 7 }
